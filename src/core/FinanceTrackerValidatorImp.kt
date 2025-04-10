56899e18f56e16816f6ee6b254ac86d5a37571aa
Mostafa Abdelfatah Elwishy <mostafaelweshy777@gmail.com>
2025-04-10 16:52:23 +0200

    implement validate transaction function in FinanceTrackerValidatorImp class - validate if amount less than or equal 0 - validate if title is an empty string

--- src/core/FinanceTrackerValidatorImp.kt
+++ src/core/FinanceTrackerValidatorImp.kt
@@ -2,9 +2,15 @@ package core
 
 import models.Transaction
 
-class FinanceTrackerValidatorImp: FinanceTrackerValidator {
+class FinanceTrackerValidatorImp :FinanceTrackerValidator{
     //todo: implement methods to validate transactions and reports
     override fun validateTransaction(transaction: Transaction): Boolean {
+        if(transaction.amount <= 0) {
+            return false
+        }
+        else if(transaction.title.isEmpty()) {
+            return false
+        }
         return true
     }
 }
