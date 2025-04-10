19e8963db501976f84bb5dca3894c06323d2bad1
Mohamed Skaik <mohamedsaeb.skaik@gmail.com>
2025-04-10 15:02:10 +0200

    add implementation to balance report

--- src/core/DefaultFinanceTrackerViewer.kt
+++ src/core/DefaultFinanceTrackerViewer.kt
@@ -2,7 +2,6 @@ package core
 
 import datasource.FinanceTrackerDataSource
 import models.*
-import java.util.*
 
 class DefaultFinanceTrackerViewer(private val dataSource: FinanceTrackerDataSource) : FinanceTrackerViewer {
 
--- src/models/TotalTransactions.kt
+++ src/models/TotalTransactions.kt
@@ -4,17 +4,17 @@ data class TotalTransactions(
     var transactions: List<Transaction>,
 ) {
 
-    var incomeBalance: Double = 0.0
+    val incomeBalance: Double = 0.0
         get() {
             val incomeTransactions = transactions.filter { it.transactionType == TransactionType.INCOME }
             return incomeTransactions.sumOf { it.amount }
         }
-    var expensesBalance: Double = 0.0
+    val expensesBalance: Double = 0.0
         get() {
             val expensesTransactions = transactions.filter { it.transactionType == TransactionType.EXPENSE }
             return expensesTransactions.sumOf { it.amount }
         }
-    var totalBalance: Double = 0.0
+    val totalBalance: Double = 0.0
         get() = incomeBalance - expensesBalance
 
 }
