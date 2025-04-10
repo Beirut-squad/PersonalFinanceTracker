fb334f3a1699dec8fe1bec7f09e49f5a760dc9cb
yousef-osama11 <oyousef709@gmail.com>
2025-04-10 17:12:02 +0200

    implement addTransaction -made by Mostafa Khaled and Yousef Osama

--- src/models/Transaction.kt
+++ src/models/Transaction.kt
@@ -1,12 +1,23 @@
 package models
 
+import java.time.LocalDate
 import java.util.Date
 import java.util.UUID
 data class Transaction(
-    val id: Int,
+    val id: Int = getNextId(),
     val title: String,
     val amount: Double,
     val transactionType: TransactionType,
     val category: Category,
-    val date: Date
-)
+    val date: Date = Date()
+){
+    companion object {
+        private var lastId = 0
+
+        fun getNextId(): Int {
+            lastId++
+            return lastId
+        }
+    }
+
+}
--- src/test/main.kt
+++ src/test/main.kt
@@ -4,17 +4,18 @@ import core.FinanceTrackerManager
 import core.FinanceTrackerManagerImpl
 import datasource.FinanceTrackerDataSource
 import datasource.InMemoryDataSource
+import models.Category
+import models.Transaction
+import models.TransactionType
+import java.util.Date
 
 
 fun main() {
 
 
-
-
     val ftDataSource: FinanceTrackerDataSource = InMemoryDataSource()
     val financeTrackerManager: FinanceTrackerManager = FinanceTrackerManagerImpl(ftDataSource)
     val addChecker = AddTransactionChecker(financeTrackerManager)
-
 
 
     val deleteChecker = DeleteTransactionChecker(
