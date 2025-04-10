5584803e8ed5af5e4ac9c3ed19ca36e6aec1ec74
Mohamed Skaik <mohamedsaeb.skaik@gmail.com>
2025-04-10 15:06:32 +0200

    add implementation to balance report

--- src/models/TotalTransactions.kt
+++ src/models/TotalTransactions.kt
@@ -3,18 +3,18 @@ package models
 data class TotalTransactions(
     var transactions: List<Transaction>,
 ) {
-
-    val incomeBalance: Double = 0.0
+    val incomeBalance: Double
         get() {
             val incomeTransactions = transactions.filter { it.transactionType == TransactionType.INCOME }
             return incomeTransactions.sumOf { it.amount }
         }
-    val expensesBalance: Double = 0.0
+
+    val expensesBalance: Double
         get() {
             val expensesTransactions = transactions.filter { it.transactionType == TransactionType.EXPENSE }
             return expensesTransactions.sumOf { it.amount }
         }
-    val totalBalance: Double = 0.0
-        get() = incomeBalance - expensesBalance
 
+    val totalBalance: Double
+        get() = incomeBalance - expensesBalance
 }
