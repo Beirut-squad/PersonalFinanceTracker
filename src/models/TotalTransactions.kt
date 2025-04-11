5ac28931ba09a8a1d8750db11e61172f15838840
Bassant Mohammed <bassant.22230@gmail.com>
2025-04-11 13:44:59 +0200

    implement balance report for ui

--- src/models/TotalTransactions.kt
+++ src/models/TotalTransactions.kt
@@ -3,13 +3,13 @@ package models
 data class TotalTransactions(
     var transactions: List<Transaction>,
 ) {
-    private val incomeBalance: Double
+    val incomeBalance: Double
         get() {
             val incomeTransactions = transactions.filter { it.transactionType == TransactionType.INCOME }
             return incomeTransactions.sumOf { it.amount }
         }
 
-    private val expensesBalance: Double
+    val expensesBalance: Double
         get() {
             val expensesTransactions = transactions.filter { it.transactionType == TransactionType.EXPENSE }
             return expensesTransactions.sumOf { it.amount }
@@ -17,4 +17,5 @@ data class TotalTransactions(
 
     val totalBalance: Double
         get() = incomeBalance - expensesBalance
+
 }
